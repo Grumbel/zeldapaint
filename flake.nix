7fa1b567d64f72da68dae3e6a247ffeaa7774345
Ingo Ruhnke <grumbel@gmail.com>
2023-05-29 21:48:58 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Very simple tile editor";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,22 +10,28 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree {
+      in {
+        packages = rec {
+          default = zeldapaint;
+
           zeldapaint = pkgs.stdenv.mkDerivation {
             pname = "zeldapaint";
             version = "0.0.0";
-            src = nixpkgs.lib.cleanSource ./.;
+
+            src = ./.;
+
             enableParallelBuilding = true;
-            nativeBuildInputs = [
-              pkgs.cmake
-              pkgs.pkg-config
+
+            nativeBuildInputs = with pkgs; [
+              cmake
+              pkg-config
             ];
-            buildInputs = [
-              pkgs.allegro4
+
+            buildInputs = with pkgs; [
+              allegro4
             ];
            };
         };
-        defaultPackage = packages.zeldapaint;
-      });
+      }
+    );
 }
